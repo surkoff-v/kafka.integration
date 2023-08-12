87c84cb822130e8ca34435f2716c20889f5c0608
vladimir.surcov <vladimir.surcov@dsi.md>
2023-08-12 16:59:04 +0300

    version up

--- src/main/java/com/vs/kafka/integration/services/MessagesService.java
+++ src/main/java/com/vs/kafka/integration/services/MessagesService.java
@@ -12,12 +12,9 @@ import org.springframework.kafka.support.SendResult;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.time.LocalDateTime;
-import java.time.chrono.ChronoLocalDate;
-import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class MessagesService {
@@ -27,45 +24,43 @@ public class MessagesService {
     @Autowired
     private KafkaTemplate<String, Greeting> kafkaTemplate;
 
- /*   public void sendMessage(String msg) {
-        kafkaTemplate.send("vvs-topic", msg);
-    }
-*/
+    /*   public void sendMessage(String msg) {
+           kafkaTemplate.send("vvs-topic", msg);
+       }
+   */
     public void sendMessage(Greeting message) {
 
-        ListenableFuture<SendResult<String, Greeting>> future =
-                kafkaTemplate.send("vvs-topic", LocalDateTime.now().toString(), message);
+        CompletableFuture<SendResult<String, Greeting>> future = kafkaTemplate.send("vvs-topic", LocalDateTime.now().toString(), message);
 
-        future.addCallback(new ListenableFutureCallback<>() {
+        future.whenComplete((result, ex) -> {
+                    if (ex == null) {
+                        logger.info("Sent message=[" + message.getMsg() + " " + message.getName() +
+                                "] with offset=[" + result.getRecordMetadata().offset() + "]");
+                    } else {
+                        logger.error("Unable to send message=["
+                                + message + "] due to : " + ex.getMessage());
+                    }
 
-            @Override
-            public void onSuccess(SendResult<String, Greeting> result) {
-                logger.debug("Sent message=[" + message.getMsg() +" " + message.getName()+
-                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
-            }
-            @Override
-            public void onFailure(Throwable ex) {
-                logger.warn("Unable to send message=["
-                        + message + "] due to : " + ex.getMessage());
-            }
-        });
+
+                }
+        );
     }
 
     @KafkaListener(topics = "vvs-topic", groupId = "foo")
     public void listenGroupFoo(@Payload Greeting message
-            ,@Header(KafkaHeaders.GROUP_ID) String groupId
-            ,@Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key
-            ,@Header(KafkaHeaders.RECEIVED_PARTITION_ID) Integer partition
-            ,@Header(KafkaHeaders.RECEIVED_TOPIC) String topic
-            ,@Header(KafkaHeaders.OFFSET) Long offset
-            ,Acknowledgment ack) {
+            , @Header(KafkaHeaders.GROUP_ID) String groupId
+            , @Header(KafkaHeaders.RECEIVED_KEY) String key
+            , @Header(KafkaHeaders.RECEIVED_PARTITION) Integer partition
+            , @Header(KafkaHeaders.RECEIVED_TOPIC) String topic
+            , @Header(KafkaHeaders.OFFSET) Long offset
+            , Acknowledgment ack) {
 
-        logger.debug("Received message " +  message.getMsg() +" " + message.getName());
-        logger.debug("groupId "+ groupId);
-        logger.debug("key "+ key);
-        logger.debug("partition "+ partition);
-        logger.debug("topic "+ topic);
-        logger.debug("offset "+ offset);
+        logger.debug("Received message " + message.getMsg() + " " + message.getName());
+        logger.debug("groupId " + groupId);
+        logger.debug("key " + key);
+        logger.debug("partition " + partition);
+        logger.debug("topic " + topic);
+        logger.debug("offset " + offset);
 
         ack.acknowledge();
     }
